69d547a9cece753ab9e95229b57911b2e39b6e41
Alex <aleksandrstp1>
2018-10-01 16:35:29 +0300

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -3,26 +3,27 @@ package ru.javawebinar.topjava.model;
 import java.time.LocalDateTime;
 
 public class UserMeal {
+    private final LocalDateTime dateTime;
 
-private final LocalDateTime dateTime;
-private final String description;
-private final int calories;
+    private final String description;
+
+    private final int calories;
 
     public UserMeal(LocalDateTime dateTime, String description, int calories) {
         this.dateTime = dateTime;
-       this.description = description;
+        this.description = description;
         this.calories = calories;
     }
 
-  public LocalDateTime getDateTime() {
+    public LocalDateTime getDateTime() {
         return dateTime;
-   }
+    }
 
     public String getDescription() {
-       return description;
-   }
+        return description;
+    }
 
     public int getCalories() {
-       return calories;
-   }
+        return calories;
+    }
 }
